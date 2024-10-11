c1a91daa196097b0ccabc590c5f6699f89965e15
34.YUHAIR IRSYAD X PPLG 3 <irsyad.jaa123@gmail.com>
2024-10-11 16:20:22 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\adminController;
+use App\Http\Controllers\dashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/', function () {
+//     return view('admin.main');
+// });
+
+//  dashboard ADMIN
+Route::get('/', [adminController::class, 'index'])->name('index');
+
+// admin
+Route::get('/quotes', [adminController::class, 'quotes'])->name('quotes');
