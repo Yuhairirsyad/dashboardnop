cd0bfc2859de520d1d8605c72457006a0f8a75e4
Satura Izhato Rufi <140029924+AyamGorengMadura@users.noreply.github.com>
2024-10-17 15:43:59 +0700

    dashboard grub user v.1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Datachallenge;
+use App\Models\Listdaftar;
+use App\Models\Quotes;
+use App\Models\Input;
+use App\Models\GrupChallenges;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DashboardController extends Controller
 {
     public function dsbusr(){
         return view('user');
+    }
+    public function dashgrub(){
+        $data = listdaftar::all();
+        return view('dashgrub', compact('data'));
+
     }
 }
